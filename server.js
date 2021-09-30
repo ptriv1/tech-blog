fc736c1fdb316775ffe8dc155360b65c9ea33a06
Galatia <ptrivedius@aol.com>
2021-09-30 07:53:11 -0400

    edited file structure and added routes

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const apth = require('path');
-const hbs = exphbs.create({});
+const path = require('path');
+const routes = require("./controllers");
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine(".hbs", exphbs({ extname: ".hbs"}));
+app.set("view engine", ".hbs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/routes'));
+/* app.use(require('./controllers/routes')); */
+
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log('Server listening on http://localhost:' + PORT);
