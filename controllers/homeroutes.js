64ae50c10bb89506aa2c38e87b1a07dd1c6f118d
Galatia <ptrivedius@aol.com>
2021-12-03 16:40:33 -0500

    pushing current code

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -55,7 +55,7 @@ router.get('/profile', withAuth, async (req, res) => {
 
         const user = userData.get({ plain: true });
 
-        res.render('profile', {
+        res.render('/profile', {
             ...user,
             logged_in: true
         });
