f13951b269ad0b1f1854fd4b7755e9bda8efb979
Galatia <ptrivedius@aol.com>
2021-12-03 19:15:04 -0500

    pushing current code

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Post, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
     try {
         const postData = await Post.findAll({
             include: [
