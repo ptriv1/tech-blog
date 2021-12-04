fe8aa715e8ce3ea0f96e5fe983d7816120e2e963
Galatia <ptrivedius@aol.com>
2021-12-03 23:30:07 -0500

    pushing current code

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
 const apiRoutes = require('./api');
-const routes = require('./routes');
+const homeRoutes = require('./homeRoutes');
 
-router.use('/', routes);
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
